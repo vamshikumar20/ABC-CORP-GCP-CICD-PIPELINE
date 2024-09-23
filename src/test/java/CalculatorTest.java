08ac34969970152bb837b77dd2748505853b499b
asecurityguru <100665517+asecurityguru@users.noreply.github.com>
2024-09-23 18:32:22 +0530

    Update CalculatorTest.java

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -9,18 +9,18 @@ import org.junit.Test;
 
 public class CalculatorTest {
 
-	@Test
+    @Test
     public void verifyAdditionOne() {
 
-		Calculator calculator = new Calculator();
+	Calculator calculator = new Calculator();
         int sum = calculator.addition("5+2");
         assertEquals(7, sum);
     }
 	
-	@Test
+    @Test
     public void verifyAdditionTwo() {
 
-		Calculator calculator = new Calculator();
+	Calculator calculator = new Calculator();
         int sum = calculator.addition("13+27");
         assertEquals(40, sum);
     }
